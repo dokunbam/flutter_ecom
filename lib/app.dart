df55d34c844eb578842ef50c4e7ca7b992f4b181
Olu <portcalladmin@portcall.com>
2023-11-11 19:38:44 -0600

    onboarding screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,17 @@
+import 'package:ecommerce/features/authentication/screens/onboarding.dart';
 import 'package:ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
+    return GetMaterialApp(
+        themeMode: ThemeMode.system,
+        theme: TAppTheme.lightTheme,
+        darkTheme: TAppTheme.darkTheme,
+        home: const OnBoardingScreen());
   }
 }
---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  //variables
+
+  ///Update Current Index when Page Scroll
+  void updatePageIndicator(index) {}
+
+  ///Jump to the specific dot selected page
+  void dotNavigateClick(index) {}
+
+  void nextPage() {}
+
+  void skipPage() {}
+}
